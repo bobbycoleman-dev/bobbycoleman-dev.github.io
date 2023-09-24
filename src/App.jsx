c228008260723436dd052d9a73ba44c7f26c03c3
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-09-23 21:24:54 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+import About from "./views/About";
+import Hero from "./views/Hero";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 function App() {
-  const [count, setCount] = useState(0)
+	useEffect(() => {
+		themeChange(false);
+		// 👆 false parameter is required for react project
+	}, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<div className="fixed w-full">
+					<Navbar />
+				</div>
+				<Hero />
+				<About />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
